896c9baddaa7cdd953535c92ca2bd6850c28a7e4
Samarth Sonawane <samarth142sonawane@gmail.com>
2026-06-01 23:58:40 +0530

    Program for finding Largest and second largest element using Array

---
+++ Arrays/largest_and_second_largest_element.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[] = {12, 45, 7, 89, 34};
+    int n = 5;
+
+    int largest = arr[0];
+    int secondLargest = arr[0];
+
+    for(int i = 1; i < n; i++) {
+
+        if(arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+
+        else if(arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    cout << "Largest Element = " << largest << endl;
+    cout << "Second Largest Element = " << secondLargest;
+
+    return 0;
+}
