4d68881ca37c26658696f73488d7d293f302c564
Samarth Sonawane <samarth142sonawane@gmail.com>
2026-06-10 22:46:45 +0530

    Added binary search solution

---
+++ Arrays/Binary_search.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+
+    int left = 0;
+    int right = n - 1;
+
+    while(left <= right) {
+
+        int mid = (left + right) / 2;
+
+        if(arr[mid] == target) {
+            return mid;
+        }
+        else if(arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+
+    int arr[] = {1, 3, 5, 7, 9, 11, 13};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 9;
+
+    int result = binarySearch(arr, n, target);
+
+    if(result != -1) {
+        cout << "Element found at index " << result;
+    }
+    else {
+        cout << "Element not found";
+    }
+
+    return 0;
+}
